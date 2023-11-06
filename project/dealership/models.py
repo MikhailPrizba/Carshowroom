60f95cb3c63cdf9d5235ccd33ed06e3e555b85f5
mikhail <mishaprizby@gmail.com>
2023-11-06 14:15:52 +0300

    Add dealership model

--- project/dealership/models.py
+++ project/dealership/models.py
@@ -1,3 +1,33 @@
+from common.models import (
+    CarInformationMixin,
+    MainInformationMixin,
+    UserInformationMixin,
+)
+from customer.models import Customer
+from django.core.validators import MinValueValidator
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Dealership(MainInformationMixin, UserInformationMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(
+        default=0, max_digits=12, decimal_places=2, validators=[MinValueValidator(0.00)]
+    )
+
+
+class ShopCar(MainInformationMixin, CarInformationMixin):
+    dealership = models.ForeignKey(Dealership, on_delete=models.CASCADE)
+    price = models.DecimalField(
+        default=0, max_digits=12, decimal_places=2, validators=[MinValueValidator(0.00)]
+    )
+    count = models.IntegerField(default=1, validators=[MinValueValidator(0)])
+
+
+class ShopSale(MainInformationMixin):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    dealership = models.ForeignKey(Dealership, on_delete=models.CASCADE)
+    max_price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00)]
+    )
+    paid = models.BooleanField(default=False)
