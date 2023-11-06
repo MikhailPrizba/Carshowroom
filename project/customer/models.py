13afbd19270a8b5e696de4321eb0aaee00affe9e
mikhail <mishaprizby@gmail.com>
2023-11-06 14:15:17 +0300

    Add Customer model

--- project/customer/models.py
+++ project/customer/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from common.models import MainInformationMixin, UserInformationMixin
+from user.models import User
+
+
+
+
+
+class Customer(MainInformationMixin, UserInformationMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(default=0, max_digits=12, decimal_places=2, validators=[MinValueValidator(0.00)])
+
